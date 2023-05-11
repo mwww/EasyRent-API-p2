2629d5db259903cb41509d38b60db4062cfbfe6f
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-11 10:40:56 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const db = require("./db/connection");
+const app = express();
+
+db.sequelize
+  .sync({ force: true })
+  .then(() => console.log("db syncronized!"))
+  .catch((err) => console.log("cannot syncronize db", err));
+
+app.use(cors());
+app.use(morgan("tiny"));
+
+app.get("/user", (req, res) => {});
+
+app.listen(3001, () => {
+  console.log("Running in 3001");
+});
