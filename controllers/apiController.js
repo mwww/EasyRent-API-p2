f75150fac4e74c565c634c218dc91d00a8962b08
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-13 22:22:57 +0700

    Update apiController.js

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,29 +3,37 @@
 const db = require('../db/connection')
 
 const getCars = async (req, res) => {
-  const car = await db.cars.findAll();
+  const carsData = await db.cars.findAll()
+  const transmissionsData = await db.transmissions.findAll()
+  const finalData = carsData.map((car) => {
+    const transmissions = transmissionsData.filter(
+      (t) => t.id_mobil === car.id_mobil
+    )
+    return {
+      ...car.toJSON(),
+      transmissions,
+    }
+  })
   return res.json({
-      status: 200,
-      message: "Success get all data",
-      data: car
-  });
+    status: 200,
+    message: 'Success get all data',
+    data: finalData,
+  })
 }
 
 const getCarsSortBy = async (req, res) => {
   const sortby = req.query.sortby
   const direction = req.query.direction
-  
+
   let carsData = await db.cars.findAll({
-    order: [
-      [sortby, direction]
-    ]
-  });
+    order: [[sortby, direction]],
+  })
 
   return res.json({
     status: 200,
-    message: "Success get all data",
-    data: carsData
-  });
+    message: 'Success get all data',
+    data: carsData,
+  })
 
   // res.send({
   //   'sortby': sortby,
