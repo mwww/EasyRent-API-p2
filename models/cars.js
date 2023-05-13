568ac93bf8674e5a5d2dd20e1be3af211c950452
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-13 13:29:31 +0700

    Update cars.js

--- models/cars.js
+++ models/cars.js
@@ -64,5 +64,35 @@ module.exports = (sequelize, DataTypes) => {
     }
   )
 
+  const fs = require('fs')
+
+  const rawJsonData = JSON.parse(fs.readFileSync('sample_data.json'))
+  const carJsonData = () => {
+    var r = []
+    rawJsonData.forEach((el) => {
+      r.push({
+        id_mobil: el.id_mobil,
+        popularity_idx: popularity_idx,
+        model: el.model,
+        brand: el.brand,
+        release_year: el.release_year,
+        price: el.price,
+        engine: el.engine,
+        HP: el.HP,
+        TRQ: el.TRQ,
+      })
+    })
+    return r
+  }
+
+  cars
+    .bulkCreate(carJsonData)
+    .then(() => {
+      console.log('Data migrated successfully!')
+    })
+    .catch((err) => {
+      console.error('Error while migrating data:', err)
+    })
+
   return cars
 }
