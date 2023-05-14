93943a97b260d677fd4f2f14310ac80ffc0a5890
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-14 09:15:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
 
-const apiRoute = require("./routes/apiRoute");
+const apiRoute = require('./routes/apiRoute')
 
-const db = require("./db/connection");
-const app = express();
-dotenv.config();
+const db = require('./db/connection')
+const app = express()
+dotenv.config()
 
 // const migrate_json_to_db = require('./features/bulkCreate/migrate_json_to_db')
 // migrate_json_to_db()
@@ -24,14 +24,14 @@ db.sequelize
   .catch((err) => console.log('cannot syncronize db', err))
 
 // app.use(cors())
-app.use(cors({ credentials: true }));
-app.use(morgan("tiny"));
-app.use(cookieParser());
-app.use(express.json());
+// app.use(cors({ credentials: true }));
+app.use(morgan('tiny'))
+app.use(cookieParser())
+app.use(express.json())
 
 // app.get("/user", (req, res) => {});
-app.use("/api", apiRoute);
+app.use('/api', apiRoute)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Running in ${process.env.PORT}`);
-});
+  console.log(`Running in ${process.env.PORT}`)
+})
