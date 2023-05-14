d3e9ba5101d1e139d149b1c38235c0030770d857
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-14 10:16:52 +0700

    Update migrate_json_to_db.js

--- features/bulkCreate/migrate_json_to_db.js
+++ features/bulkCreate/migrate_json_to_db.js
@@ -1,69 +1,35 @@
-// async function main(db) {
-//   const rawJsonData = require('./sample_data.json')
-//   const carJsonData = () => {
-//     var r = []
-//     rawJsonData.forEach((el) => {
-//       r.push({
-//         id_mobil: el.id_mobil,
-//         popularity_idx: el.popularity_idx,
-//         model: el.model,
-//         brand: el.brand,
-//         release_year: el.release_year,
-//         price: el.price,
-//         engine: el.engine,
-//         HP: el.HP,
-//         TRQ: el.TRQ,
-//       })
-//     })
-//     return r
-//   }
-
-//   // console.log(carJsonData())
-
-//   db.cars
-//     .bulkCreate(carJsonData())
-//     .then(() => {
-//       console.log('Data migrated successfully!')
-//     })
-//     .catch((err) => {
-//       console.error('Error while migrating data:', err)
-//     })
-//   return
-// }
-
-// module.exports = main
-
 const rawJsonData = require('./sample_data.json')
-
-// function getCarsData() {
-//   // return rawJsonData.map((data) => ({
-//   //   ...data,
-//   // }))
-//   return rawJsonData.map(({ transmission, ...data }) => data)
-// }
-
-// console.log(getCarsData())
 
 const carsData = rawJsonData.map(({ transmissions, ...car }) => car)
 
-// console.log(carsData)
-
-let counter = 1
+let counter
+counter = 1
 const transmissionsData = rawJsonData.flatMap(({ id_mobil, transmissions }) =>
-  transmissions.map((transmission, index) => ({
-    transmission_id: counter++,
+  transmissions.map((transmission) => ({
+    id_transmission: counter++,
     id_mobil,
     ...transmission,
   }))
 )
 
+counter = 1
+const gambarData = rawJsonData.flatMap(({ id_mobil, images }) =>
+  images.map((gambar) => ({
+    id_gambar: counter++,
+    id_mobil,
+    ...gambar,
+  }))
+)
+
 // console.log(carsData)
-console.log(transmissionsData)
+// console.log(transmissionsData)
+// console.log(gambarData)
 
 async function main(db) {
   try {
     await db.cars.bulkCreate(carsData)
     await db.transmissions.bulkCreate(transmissionsData)
+    await db.images.bulkCreate(gambarData)
     console.log('Data migrated successfully!')
   } catch (err) {
     console.error('Error while migrating data:', err)
