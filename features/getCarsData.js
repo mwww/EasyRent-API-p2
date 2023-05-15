1e0f5b37884667eccff28a1e91df11967298ae5f
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-15 21:35:25 +0700

    Update getCarsData.js

--- features/getCarsData.js
+++ features/getCarsData.js
@@ -1,113 +1,28 @@
 const db = require('../db/connection')
 
-function getCarsData() {
-  // const carsData = [
-  //   {
-  //     EasyRentData: {
-  //       ID: 1,
-  //       PopularityIndex: 6.5,
-  //       Imgs: ['2.jpg'],
-  //     },
-  //     CarData: {
-  //       Brand: 'Chevrolet',
-  //       Model: 'Corvette Stingray',
-  //       ReleaseYear: 2023,
-  //       Transmissions: {
-  //         AT: '8-speed dual-clutch',
-  //         MT: '7-speed',
-  //       },
-  //       Engine: '6.2-liter naturally aspirated V8',
-  //       Power: {
-  //         HP: 495,
-  //         TQ: 470,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     EasyRentData: {
-  //       ID: 2,
-  //       PopularityIndex: 8.7,
-  //       Imgs: ['1.jpg'],
-  //     },
-  //     CarData: {
-  //       Brand: 'Porsche',
-  //       Model: 'Porsche 718 Cayman GT4',
-  //       ReleaseYear: 2023,
-  //       Transmissions: {
-  //         MT: '6-speed',
-  //       },
-  //       Engine: '4.0-liter naturally aspirated flat-six',
-  //       Power: {
-  //         HP: 414,
-  //         TQ: 309,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     EasyRentData: {
-  //       ID: 3,
-  //       PopularityIndex: 7.9,
-  //       Imgs: ['3.jpg'],
-  //     },
-  //     CarData: {
-  //       Brand: 'Ford',
-  //       Model: 'Mustang Mach-E GT',
-  //       ReleaseYear: 2022,
-  //       Transmissions: {
-  //         AT: 'Electric',
-  //       },
-  //       Engine: 'Dual electric motors',
-  //       Power: {
-  //         HP: 480,
-  //         TQ: 634,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     EasyRentData: {
-  //       ID: 4,
-  //       PopularityIndex: 1,
-  //       Imgs: ['1.jpg'],
-  //     },
-  //     CarData: {
-  //       Brand: 'Porsche',
-  //       Model: 'Porsche 911',
-  //       ReleaseYear: 2022,
-  //       Transmissions: {
-  //         AT: '8-speed',
-  //         MT: '7-speed',
-  //       },
-  //       Engine: '3.0-liter twin-turbocharged flat-six',
-  //       Power: {
-  //         HP: 572,
-  //         TQ: 553,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     EasyRentData: {
-  //       ID: 5,
-  //       PopularityIndex: 9.2,
-  //       Imgs: ['1.jpg'],
-  //     },
-  //     CarData: {
-  //       Brand: 'Lamborghini',
-  //       Model: 'Huracán STO',
-  //       ReleaseYear: 2022,
-  //       Transmissions: {
-  //         AT: '7-speed dual-clutch',
-  //       },
-  //       Engine: '5.2-liter naturally aspirated V10',
-  //       Power: {
-  //         HP: 640,
-  //         TQ: 417,
-  //       },
-  //     },
-  //   },
-  // ]
+async function getCarsData() {
+  const carsData = await db.cars.findAll()
+  let transmissionsData = await db.transmissions.findAll()
+  const imagesData = await db.images.findAll()
 
-  const carsData = db.cars.findAll()
-  return carsData
+  const finalData = carsData.map((car) => {
+    let transmissions = transmissionsData
+      .filter((t) => t.id_mobil === car.id_mobil)
+      .flatMap((transmission) => ({
+        transmission_type: transmission.transmission_type,
+        speed: transmission.speed,
+      }))
+    let images = imagesData
+      .filter((t) => t.id_mobil === car.id_mobil)
+      .flatMap((img) => `${img.img_name}.${img.img_ext}`)
+    return {
+      ...car.toJSON(),
+      transmissions,
+      images,
+    }
+  })
+
+  return finalData
 }
 
-module.exports = getCarsData;
+module.exports = getCarsData
