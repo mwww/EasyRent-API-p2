c55a44fd96ea2c2379542438c4c91639246c8679
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-16 01:35:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
 
-const apiRoute = require("./routes/apiRoute");
-const userRoute = require("./routes/userRoute");
+const apiRoute = require('./routes/apiRoute')
+const userRoute = require('./routes/userRoute')
 
-const db = require("./db/connection");
-const app = express();
-dotenv.config();
+const db = require('./db/connection')
+const app = express()
+dotenv.config()
 
 db.connection
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
-    console.log("db syncronized!");
-    require("./features/bulkCreate/migrate_json_to_db")(db);
+    console.log('db syncronized!')
+    require('./features/bulkCreate/migrate_json_to_db')(db)
   })
-  .catch((err) => console.log("cannot syncronize db", err));
+  .catch((err) => console.log('cannot syncronize db', err))
 
 // app.use(cors())
-app.use(cors({ credentials: true }));
-app.use(morgan("tiny"));
-app.use(cookieParser());
-app.use(express.json());
+app.use(cors({ credentials: true }))
+app.use(morgan('tiny'))
+app.use(cookieParser())
+app.use(express.json())
 
 // app.get("/user", (req, res) => {});
-app.use("/api", apiRoute);
-app.use("/user", userRoute);
+app.use('/api', apiRoute)
+app.use('/user', userRoute)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Running in ${process.env.PORT}`);
-});
+  console.log(`Running in ${process.env.PORT}`)
+})
