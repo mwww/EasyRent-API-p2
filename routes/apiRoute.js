3efde36c77b397519a9eb67a8fffd4789a66dea8
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-15 18:44:10 +0700

    Update apiRoute.js

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const apiController = require("../controllers/apiController");
-const usersControler = require("../controllers/usersControler");
-const verifyToken = require("../middleware/verifyToken");
-const refreshToken = require("../controllers/refreshToken");
+const express = require('express')
+const router = express.Router()
+const apiController = require('../controllers/apiController')
+const usersControler = require('../controllers/usersControler')
+const verifyToken = require('../middleware/verifyToken')
+const refreshToken = require('../controllers/refreshToken')
 // const verifyToken = require("../middleware/verifyToken");
 // const {
 //   getUsers,
@@ -18,9 +18,8 @@ const refreshToken = require("../controllers/refreshToken");
 // const Logout = require('../controllers/usersControler');
 // const {getUsers} = require("../controllers/usersControler")
 
-router.get("/cars", apiController.getCars);
-router.get("/cars/sortby/:sortby", apiController.getCarsSortBy);
-
+router.get('/cars', apiController.getCars)
+router.get('/cars/sortby/:sortby', apiController.getCarsSortBy)
 
 // router.get('/cars/sortby/:sortby&:direction', apiController.getCarsSortBy)
 
@@ -28,11 +27,11 @@ router.get("/cars/sortby/:sortby", apiController.getCarsSortBy);
 // localhost.../cars?sortby=ID
 // router.get('/cars/sortby/:direction/:sortby', apiController.getCarsSortBy)
 
-router.get("/users", verifyToken.verifyToken, usersControler.getUsers);
-router.post("/users", usersControler.Register);
-router.post("/login", usersControler.Login);
-router.get("/token", refreshToken.refreshToken);
+router.get('/users', verifyToken.verifyToken, usersControler.getUsers)
+router.post('/users', usersControler.Register)
+router.post('/login', usersControler.Login)
+router.get('/token', refreshToken.refreshToken)
 // router.delete("/logout", Logout);
 // console.log(getUsers());
 
-module.exports = router;
+module.exports = router
