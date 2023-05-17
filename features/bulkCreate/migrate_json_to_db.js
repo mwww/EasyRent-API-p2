7dc55d9dd99276841f622602dbd1cbb1ae7b4cab
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-17 13:53:34 +0700

    Update migrate_json_to_db.js

--- features/bulkCreate/migrate_json_to_db.js
+++ features/bulkCreate/migrate_json_to_db.js
@@ -1,29 +1,20 @@
 const rawJsonData = require('./sample_data.json')
 
-const carsData = rawJsonData.map(({ transmissions, ...car }) => car)
+const carsData = rawJsonData.map(({ transmissions, images, ...car }) => car)
 
-let counter
-counter = 1
 const transmissionsData = rawJsonData.flatMap(({ id_mobil, transmissions }) =>
   transmissions.map((transmission) => ({
-    id_transmission: counter++,
     id_mobil,
     ...transmission,
   }))
 )
 
-counter = 1
 const gambarData = rawJsonData.flatMap(({ id_mobil, images }) =>
   images.map((gambar) => ({
-    id_gambar: counter++,
     id_mobil,
     ...gambar,
   }))
 )
-
-// console.log(carsData)
-// console.log(transmissionsData)
-// console.log(gambarData)
 
 async function main(db) {
   try {
