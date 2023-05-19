a9f1e0b8be961bde84314e4d725e6b5008de96b1
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-19 17:46:32 +0700

    Update adminRoute.js

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const adminController = require('../controllers/adminController')
+const adminController = require('../controllers/adminCarController')
 
 router.get('/cars', adminController.getCars)
-// router.get('/car/:id', adminController.getCar)
+router.get('/car/:id', adminController.getCar)
+router.put('/car/:id', adminController.editCar)
+router.post('/car/', adminController.addCar)
+router.delete('/car/:id', adminController.deleteCar)
+router.get('/cars/compiled', adminController.getCarsCompiled)
+router.get('/car/compiled/:id', adminController.getCarCompiled)
 
 module.exports = router
