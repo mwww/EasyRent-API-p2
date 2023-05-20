3035c80e7f8dd83c3041b71a0d3e18d4a0ee5a4d
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-20 23:50:45 +0700

    import and implement controllers for admin

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const adminController = require('../controllers/adminCarController')
-const carController = require('../controllers/admin/car')
-const transmissionController = require('../controllers/admin/transmission')
 
-router.get('/cars', carController.getCars)
+const carController = require('../controllers/admin/car/car')
+const transmissionController = require('../controllers/admin/car/transmission')
+const imageController = require('../controllers/admin/car/images')
+
+const appointmentController = require('../controllers/admin/appointment/appointment')
+
+router.get('/car', carController.getCars)
 router.get('/car/:id', carController.getCar)
 router.put('/car/:id', carController.editCar)
 router.post('/car/', carController.addCar)
 router.delete('/car/:id', carController.deleteCar)
 
-router.get('/transmissions', transmissionController.getTransmissions)
+router.get('/transmission', transmissionController.getTransmissions)
 router.get('/transmission/:id', transmissionController.getTransmission)
+router.get('/image', imageController.getImages)
+router.get('/image/:id', imageController.getImage)
+
+router.get('/appointment', appointmentController.getAppointments)
+router.get('/appointment/:id', appointmentController.getAppointment)
+router.put('/appointment/:id', appointmentController.editAppointment)
+router.post('/appointment/', appointmentController.addAppointment)
+router.delete('/appointment/:id', appointmentController.deleteAppointment)
 
 module.exports = router
