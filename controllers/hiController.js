474c2c8a3e5f2edae197ab3faa1a52c2627479be
mwww dev <115621609+mwww@users.noreply.github.com>
2023-05-20 23:48:38 +0700

    update: include received data in response

--- controllers/hiController.js
+++ controllers/hiController.js
@@ -2,6 +2,13 @@ const getHi = (req, res) => {
   res.json('hi')
 }
 
+const repeatHi = (req, res) => {
+  const body = req.body
+  console.log(body)
+  res.json({ got: 'it', ...body })
+}
+
 module.exports = {
   getHi,
+  repeatHi,
 }
